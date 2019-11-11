d1ae4cac9cdf0aa53d2c800b63fc592ed1fed109
Bo Yao <bo@nearprotocol.com>
2019-11-11 10:51:16 -0800

    release 0.1.1

--- rc/__init__.py
+++ rc/__init__.py
@@ -1,3 +1,3 @@
 from rc.provider import gcloud
 from rc.machine import Machine
-from rc.util import run, RunException
+from rc.util import run, RunException, running, run_stream, handle_stream, STDERR, STDOUT, EXIT
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-rc",  # Replace with your own username
+    version="0.1.1",
+    author="Bo Yao",
+    author_email="icerove@gmail.com",
+    description="Python remote control library for programmatically control remote machines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ailisp/python-rc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: System :: Clustering",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Systems Administration"
+    ],
+    install_requires=[
+        'retry',
+        'PyYAML',
+        'humanfriendly'
+    ]
+    python_requires='>=3.6',
+)
