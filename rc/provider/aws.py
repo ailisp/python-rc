3db2a97ecda9bad809b371ff4624e56e4315937c
Bo Yao <bo@nearprotocol.com>
2020-02-21 15:49:41 -0800

    aws regions

--- rc/provider/aws.py
+++ rc/provider/aws.py
@@ -8,3 +8,11 @@ def _get_regions():
     regions = json.loads(run(
         ['aws', 'ec2', 'describe-regions', '--region', 'us-west-1']).stdout)['Regions']
     return list(map(lambda region: region['RegionName'], regions))
+
+
+def list():
+    regions = _get_regions()
+    instances = []
+    for r in regions:
+        region_instances = json.loads(run(
+            ['aws', 'ec2', 'describe-instances', '--region', r]).stdout)['Reservations'][0]
